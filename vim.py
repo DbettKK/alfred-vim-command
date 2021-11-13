785b749c2f9b815b6f3d702da892d026a6a1a5ec
zhanghanke <394739138@qq.com>
2021-11-13 15:22:12 +0800

    version 1.0

--- vim.py
+++ vim.py
@@ -3,18 +3,26 @@ import sys
 from workflow import Workflow
 import key_map
 
-reload(sys)  # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入
-sys.setdefaultencoding('utf-8')
+
+def init():
+    reload(sys)  # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入
+    sys.setdefaultencoding('utf-8')
+    key_map.init()
 
 
 def main(workflow):
-    key_map.init()
-    for k in key_map.key_map:
-        workflow.add_item(title=k, subtitle=key_map.key_map.get(k))
-    workflow.add_item(title="this is test", subtitle="this is a test")
+    args = workflow.args[0]
+    keys = key_map.key_map.keys()
+    for key in keys:
+        if args in key:
+            workflow.add_item(title=key, subtitle=key_map.key_map.get(key))
+    for v in key_map.value_map.keys():
+        if args in v:
+            workflow.add_item(title=v, subtitle=key_map.value_map.get(v))
     workflow.send_feedback()
 
 
 if __name__ == '__main__':
     wf = Workflow()
+    init()
     sys.exit(wf.run(main))
